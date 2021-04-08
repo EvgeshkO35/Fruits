b5a93275438f75d1fd7aa6299f9db988ec41af1d
EvgeshkO35 <56804253+EvgeshkO35@users.noreply.github.com>
2021-04-08 17:51:04 -0400

    Initial Commit

---
+++ Fruits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fruits
+//
+//  Created by Evgenii Lysenko on 4/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Evgenii Lysenko on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
